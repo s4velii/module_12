83297f6397f984101f0b338eeedcedd05ee75731
s4velii <44133852+s4velii@users.noreply.github.com>
2024-09-29 19:32:32 +0300

    Add files via upload

---
+++ runner_and_tournament.py
@@ -0,0 +1,52 @@
+class Runner:
+    def __init__(self, name, speed):
+        self.name = name
+        self.distance = 0
+        self.speed = speed
+
+    def run(self):
+        self.distance += self.speed * 2
+
+    def walk(self):
+        self.distance += self.speed
+
+    def __str__(self):
+        return self.name
+
+    def __eq__(self, other):
+        if isinstance(other, str):
+            return self.name == other
+        elif isinstance(other, Runner):
+            return self.name == other.name
+
+
+class Tournament:
+    def __init__(self, distance, *participants):
+        self.full_distance = distance
+        self.participants = list(participants)
+
+    def start(self):
+        finishers = {}
+        place = 1
+        while self.participants:
+            for participant in self.participants:
+                participant.run()
+                if participant.distance // 999 >= self.full_distance:
+                    finishers[place] = participant.name
+                    place += 1
+                    self.participants.remove(participant)
+
+
+
+
+        return finishers
+
+
+# r1 = Runner('Усейн', 10)
+# r2 = Runner('Андрей', 9)
+# r3 = Runner('Ник', 3)
+# t1 = Tournament(90, r1, r3)
+# t = t1.start()
+# print(t)
+
+
