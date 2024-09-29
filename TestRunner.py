d11a44058ebfe5702ab7d08955a4932f9929d824
s4velii <44133852+s4velii@users.noreply.github.com>
2024-09-29 13:10:35 +0300

    Update TestRunner.py

--- TestRunner.py
+++ TestRunner.py
@@ -10,10 +10,10 @@ class TestRunner(unittest.TestCase):
         self.assertEqual(boy.distance, 50)
 
     def test_run(self):
-        men = Runner('boy')
+        men = Runner('men')
         for _ in range(1, 11):
             men.run()
-        self.assertEqual(men.distance, 101) # FAILED (failures=1) 101 != 100
+        self.assertEqual(men.distance, 101)  # FAILED (failures=1) 101 != 100
 
     def test_challenge(self):
         child = Runner('child')
