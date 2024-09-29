afa4bef3927aa3627929e89c97aa5df9755fcafa
s4velii <44133852+s4velii@users.noreply.github.com>
2024-09-29 13:09:34 +0300

    Add files via upload

---
+++ TestRunner.py
@@ -0,0 +1,24 @@
+from main import Runner
+import unittest
+
+
+class TestRunner(unittest.TestCase):
+    def test_walk(self):
+        boy = Runner('boy')
+        for _ in range(1, 11):
+            boy.walk()
+        self.assertEqual(boy.distance, 50)
+
+    def test_run(self):
+        men = Runner('boy')
+        for _ in range(1, 11):
+            men.run()
+        self.assertEqual(men.distance, 101) # FAILED (failures=1) 101 != 100
+
+    def test_challenge(self):
+        child = Runner('child')
+        runner = Runner('runner')
+        for _ in range(1, 11):
+            child.walk()
+            runner.run()
+        self.assertNotEqual(child.distance, runner.distance)
